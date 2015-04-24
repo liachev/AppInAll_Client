be1c1430b7e801d924c2e975cc910ab9456afb10
Denis Polish <denis.polish@gmail.com>
2015-04-24 22:08:54 +0300

    merge profiles with master
    Conflicts:
    	build-utils.xml
    	ionic/src/main/code/javascripts/js/app.js
    	ionic/src/main/code/javascripts/js/controllers.js

--- ionic/src/main/code/javascripts/js/app.js
+++ ionic/src/main/code/javascripts/js/app.js
@@ -12,40 +12,39 @@ angular.module('starter', [
     'starter.controllers',
 
 
-  /* models */ // TODO: add new models to 'ionic/src/main/code/javascripts/js/modules/data/models.js'
-  'appinall.models',
-  'appinall.models.category',
+    /* models */ // TODO: add new models to 'ionic/src/main/code/javascripts/js/modules/data/models.js'
+    'appinall.models',
+    'appinall.models.category',
 
-  /* services */
-  'ParseServices',
-  'LocalStorageModule',
-  'cordova_calendar'
+    /* services */
+    'ParseServices',
+    'LocalStorageModule',
+    'cordova_calendar'
 ])
 
-.run(function($ionicPlatform, localStorageService, calendarService) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-  // Parse initialization for logging to parse.com
-  // FixMe: duplicated with ionic/src/main/code/javascripts/js/services/parse-service.js:6
-  Parse.initialize("O7eCGvKWO5BihNXJQv8zU0Ewd9a5nLJs0EBZWFjr", "Aohwuhy4j63Rs9tL4kXuc4lD8zGqv6wgrI74yXnU");
+    .run(function ($ionicPlatform, localStorageService, calendarService) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+        // Parse initialization for logging to parse.com
+        // FixMe: duplicated with ionic/src/main/code/javascripts/js/services/parse-service.js:6
+        Parse.initialize("O7eCGvKWO5BihNXJQv8zU0Ewd9a5nLJs0EBZWFjr", "Aohwuhy4j63Rs9tL4kXuc4lD8zGqv6wgrI74yXnU");
 
-  // angular localStorageService 'isSupported' test
-  if(localStorageService.isSupported)
-  {
-      logging.log("localStorageService is supported");
-  }else{
-      logging.log("localStorageService is NOT supported");
-  }
-})
+        // angular localStorageService 'isSupported' test
+        if (localStorageService.isSupported) {
+            logging.log("localStorageService is supported");
+        } else {
+            logging.log("localStorageService is NOT supported");
+        }
+    })
 
     .config(function ($stateProvider, $urlRouterProvider, $cordovaFacebookProvider) {
         $stateProvider
@@ -84,86 +83,87 @@ angular.module('starter', [
                     }
                 }
             })
-  .state('app.profiles', {
-    url: "/profiles",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/profiles.html",
-        controller: 'ProfilesCtrl',
-        resolve: {
-          delay: ['$q', '$rootScope', '$state', 'ParseSDK', function($q, $scope, $state, Parse) {
-            var delay = $q.defer();
-            var currentUser = Parse.User.current();
-            if (currentUser) {
-               // do stuff with the user
-              $scope.profiles = [];
-              var profiles = new (Parse.Collection.getClass("Profile"));
-              profiles.getProfilesByUser(currentUser).then(function (result) {
-                console.info(angular.toJson(result));
-                for (i in result) {
-                  $scope.profiles[i] = {
-                    id: result[i].id,
-                    firstName: result[i].get("firstName"),
-                    lastName: result[i].get("lastName"),
-                    avatar: result[i].get("avatar"),
-                    avatarSrc: (result[i].get("avatar") && result[i].get("avatar").url()) || "img/ionic.png",
-                    location: result[i].get("location"),
-                    gender: result[i].get("gender"),
-                    birthday: result[i].get("birthday"),
-                    kid: result[i].get("kid"),
-                    interestedIn: result[i].get("interestedIn"),
-                    name: result[i].get("firstName") + " " + result[i].get("lastName")
-                  };
-                };
-                var query = new Parse.Query(Parse.User);
-                query.get(currentUser.id, {
-                  success: function(user) {
-                    // The object was retrieved successfully.
-                    var selectedProfile = user.get("selectedProfile");
-                    selectedProfile && selectedProfile.fetch({
-                      success: function(profile) {
-                        $scope.selectedProfile = profile.id;
-                      }
-                    });
-                  },
-                  error: function(object, error) {
-                    // The object was not retrieved successfully.
-                    // error is a Parse.Error with an error code and message.
-                    alert('current user getting error: ' + angular.toJson(error, true));
-                    delay.reject(error);
-                  }
-                }).then(function (user) {
-                    delay.resolve();
-                }, function (error) {
-                    delay.reject(error);
-                });
-              });
-            } else {
-              $state.go('app.signup'); // show the signup or login page
-              delay.resolve();
-            }
-            return delay.promise;
-          }]
-        }
-      }
-    }
-  })
+            .state('app.profiles', {
+                url: "/profiles",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/profiles.html",
+                        controller: 'ProfilesCtrl',
+                        resolve: {
+                            delay: ['$q', '$rootScope', '$state', 'ParseSDK', function ($q, $scope, $state, Parse) {
+                                var delay = $q.defer();
+                                var currentUser = Parse.User.current();
+                                if (currentUser) {
+                                    // do stuff with the user
+                                    $scope.profiles = [];
+                                    var profiles = new (Parse.Collection.getClass("Profile"));
+                                    profiles.getProfilesByUser(currentUser).then(function (result) {
+                                        console.info(angular.toJson(result));
+                                        for (i in result) {
+                                            $scope.profiles[i] = {
+                                                id: result[i].id,
+                                                firstName: result[i].get("firstName"),
+                                                lastName: result[i].get("lastName"),
+                                                avatar: result[i].get("avatar"),
+                                                avatarSrc: (result[i].get("avatar") && result[i].get("avatar").url()) || "img/ionic.png",
+                                                location: result[i].get("location"),
+                                                gender: result[i].get("gender"),
+                                                birthday: result[i].get("birthday"),
+                                                kid: result[i].get("kid"),
+                                                interestedIn: result[i].get("interestedIn"),
+                                                name: result[i].get("firstName") + " " + result[i].get("lastName")
+                                            };
+                                        }
+                                        ;
+                                        var query = new Parse.Query(Parse.User);
+                                        query.get(currentUser.id, {
+                                            success: function (user) {
+                                                // The object was retrieved successfully.
+                                                var selectedProfile = user.get("selectedProfile");
+                                                selectedProfile && selectedProfile.fetch({
+                                                    success: function (profile) {
+                                                        $scope.selectedProfile = profile.id;
+                                                    }
+                                                });
+                                            },
+                                            error: function (object, error) {
+                                                // The object was not retrieved successfully.
+                                                // error is a Parse.Error with an error code and message.
+                                                alert('current user getting error: ' + angular.toJson(error, true));
+                                                delay.reject(error);
+                                            }
+                                        }).then(function (user) {
+                                            delay.resolve();
+                                        }, function (error) {
+                                            delay.reject(error);
+                                        });
+                                    });
+                                } else {
+                                    $state.go('app.signup'); // show the signup or login page
+                                    delay.resolve();
+                                }
+                                return delay.promise;
+                            }]
+                        }
+                    }
+                }
+            })
 
-           
-  .state('app.signup', {
-    url: "/signup",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/signup.html",
-        controller: 'SignUpCtrl',
-        resolve: {
-          delay: ['$q', '$http', '$rootScope', function ($q, $http, $scope) {
-            return loadTranslation($q, $http, $scope, 'signup');
-          }]
-        }
-      }
-    }
-  })
+
+            .state('app.signup', {
+                url: "/signup",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/signup.html",
+                        controller: 'SignUpCtrl',
+                        resolve: {
+                            delay: ['$q', '$http', '$rootScope', function ($q, $http, $scope) {
+                                return loadTranslation($q, $http, $scope, 'signup');
+                            }]
+                        }
+                    }
+                }
+            })
 
             .state('app.agreement', {
                 url: "/agreement",
@@ -175,35 +175,20 @@ angular.module('starter', [
                 }
             })
 
-  .state('app.profile', {
-    url: "/profiles/:profileId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/profile.html",
-        controller: 'ProfileCtrl',
-        resolve: {
-          delay: ['$q', '$http', '$rootScope', function ($q, $http, $scope) {
-            return loadTranslation($q, $http, $scope, 'profiles');
-          }]
-        }
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/signup');
-
-  var loadTranslation = function (q, http, scope, dir) {
-    var delay = q.defer();
-    http.get('translate/' + dir + '/strings.json').success(function(result) {
-      scope.strings = result;
-      delay.resolve();
-    }).error(function(object, code) {
-      console.warn(object);
-      delay.reject();
-    });
-    return delay.promise;
-  };
-            .state('app.single', {
+            .state('app.profile', {
+                url: "/profiles/:profileId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/profile.html",
+                        controller: 'ProfileCtrl',
+                        resolve: {
+                            delay: ['$q', '$http', '$rootScope', function ($q, $http, $scope) {
+                                return loadTranslation($q, $http, $scope, 'profiles');
+                            }]
+                        }
+                    }
+                }
+            }).state('app.single', {
                 url: "/playlists/:playlistId",
                 views: {
                     'menuContent': {
@@ -231,6 +216,21 @@ angular.module('starter', [
                 }
             })
         ;
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/signup');
+
+        var loadTranslation = function (q, http, scope, dir) {
+            var delay = q.defer();
+            http.get('translate/' + dir + '/strings.json').success(function (result) {
+                scope.strings = result;
+                delay.resolve();
+            }).error(function (object, code) {
+                console.warn(object);
+                delay.reject();
+            });
+            return delay.promise;
+        };
+
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/playlists');
 
